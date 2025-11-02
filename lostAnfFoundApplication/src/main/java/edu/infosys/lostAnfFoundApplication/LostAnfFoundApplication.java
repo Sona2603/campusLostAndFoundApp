ae85577e975d9dccad4bf3104bfa9feca5ee0b1f
Sona2603 <sonanagarajan076@gmail.com>
2025-11-02 21:11:42 +0530

    Initial Commit

---
+++ lostAnfFoundApplication/src/main/java/edu/infosys/lostAnfFoundApplication/LostAnfFoundApplication.java
@@ -0,0 +1,13 @@
+package edu.infosys.lostAnfFoundApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAnfFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAnfFoundApplication.class, args);
+	}
+
+}
---
+++ lostAnfFoundApplication/src/test/java/edu/infosys/lostAnfFoundApplication/LostAnfFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.infosys.lostAnfFoundApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAnfFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
